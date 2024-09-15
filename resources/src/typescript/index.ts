7d7c1b3e6e41fefb4aae983ab6848c6e3f0f1628
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-09-15 15:09:16 -0500

    added rimraf to clean dist, webpack server not rebuilding after reload

--- resources/src/typescript/index.ts
+++ resources/src/typescript/index.ts
@@ -1,8 +1,8 @@
 /// <reference types="webpack/module" /> 
-import '../css/main.css'
+import '../css/main.css';
 import  generateJoke  from "./lib/generateJoke";
 import { getPokemon } from "./lib/getPokemon";
-console.log('Recovered work!')
+console.log('Hello, Bradley Parker!')
 const joke = generateJoke();
 console.log(joke);
 // console.log(import.meta.webpack); // without reference declared above, TypeScript will throw an error
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'resources/src/typescript/index.ts'),
@@ -8,22 +7,35 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true, // Speeds up builds without type checking
+                    },
+                },
                 exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
                 include: path.resolve(__dirname, "resources/src/css"),
-                use: ['style-loader', 'css-loader', 'postcss-loader']
-            }
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts','.jsx', '.js'],
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
     },
     output: {
         path: path.resolve(__dirname, 'resources/dist'),
         filename: 'bundle.js',
     },
-}
-
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'resources/dist'),
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        watchFiles: ['resources/**/*'], // Watch all files in the resources directory
+    },
+};
